919bcc071c4dddc3be907ffe3a697b30b5c47197
ahmednabil310 <an30458@gmail.com>
2021-07-28 21:22:49 +0200

    editing carousel

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+let indicator = document.querySelectorAll('.history-timeline__list li p');
+let carouselContent = document.querySelectorAll('#carousel-content');
+console.log(indicator);
+// console.log(carouselContent);
+for (var i = 0; i < indicator.length; i++) {
+  indicator[i].addEventListener('click', function () {
+    const id = this.getAttribute('data-sectionId');
+
+    for (var i = 0; i < carouselContent.length; i++) {
+      carouselContent[i].classList.add('d-none');
+    }
+
+    document
+      .querySelector(`[data-sectionId="carousel-${id}"]`)
+      .classList.remove('d-none');
+  });
+}
