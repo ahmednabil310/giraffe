502db0f0d6036465dca4c244ad8c94a765a74193
ahmednabil310 <an30458@gmail.com>
2021-07-28 21:35:37 +0200

    finshing carousel

--- js/carousel.js
+++ js/carousel.js
@@ -6,6 +6,12 @@ for (var i = 0; i < indicator.length; i++) {
   indicator[i].addEventListener('click', function () {
     const id = this.getAttribute('data-sectionId');
 
+    for (var i = 0; i < indicator.length; i++) {
+      indicator[i].classList.remove('history-timeline__present');
+    }
+
+    this.classList.add('history-timeline__present');
+
     for (var i = 0; i < carouselContent.length; i++) {
       carouselContent[i].classList.add('d-none');
     }
